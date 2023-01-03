1c07ac5b384cc6c28b5148227a39ebdf5e9d4e52
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-01-03 15:51:59 +0500

    feat: nav_changes

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import {Box} from "@mui/system";
-import Navbar from "../documents/Reusiable/Navbar/Navbar";
+// import Navbar from "../documents/Reusiable/Navbar/Navbar";
 import React from "react";
 import Footer from "../documents/Reusiable/Footer/Footer";
 
@@ -9,7 +9,7 @@ export type LayoutType = {
 const Layout: React.FC<LayoutType> = ({children}) => {
     return (
         <Box>
-            <Navbar/>
+            {/*<Navbar/>*/}
             <Box>{children}</Box>
             <Footer />
         </Box>
