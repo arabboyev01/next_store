cbe678b7ae4725402c32e9eeada939837cb1eaca
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-01-08 14:32:45 +0500

    feat: search engine

--- src/components/SearchData/SearchData.tsx
+++ src/components/SearchData/SearchData.tsx
@@ -3,19 +3,20 @@ import {Box} from "@mui/system";
 import {useSelector} from "react-redux";
 import {setSearchValue} from "../../redux/CartSlice";
 import MainCart from "../../documents/Reusiable/MainCart/MainCart";
-import {Typography} from "@mui/material";
+import {Typography, useMediaQuery} from "@mui/material";
 import NoData from "../../../public/assets/images/no-results.png"
 import Image from "next/image";
 const SearchData = () => {
     const classes = Style();
     const searchedValue = useSelector(setSearchValue);
+    const query = useMediaQuery('@media(max-width: 600px)')
     // @ts-ignore
     const data = searchedValue.payload.cart.searchValue[searchedValue.payload.cart.searchValue.length - 1];
     return (
         <Box className={classes.searchDataWrapper}>
-            {!data ?
+            {!data || data.length === 0 ?
                 <Box className={classes.empty}>
-                    <Image src={NoData.src} alt='no data' width={130} height={130}/>
+                    <Image src={NoData.src} alt='no data' width={query ? 50 : 130} height={query ? 50 : 130}/>
                     <Typography className={classes.emptyText}>Siz so&apos;ragan mahsulot bo&apos;yicha hech narsa
                         topilmadi</Typography>
                 </Box>
--- src/components/SearchData/search.style.tsx
+++ src/components/SearchData/search.style.tsx
@@ -14,11 +14,17 @@ export default makeStyles(() => ({
         justifyContent: "center",
         alignItems: "center",
         padding: "18vh 0",
+        '@media(max-width: 600px)': {
+            padding: "8vh 0",
+        },
     },
     emptyText: {
         marginTop: "30px",
         fontSize: "20px",
         fontWeight: 500,
         lineHeight: '24px',
+        '@media(max-width: 600px)': {
+            textAlign:"center",
+        },
     },
 }))
