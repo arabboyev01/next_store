fff88e40372a6340d63065d657df312bbad9ca67
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-01-18 23:22:33 +0500

    feat: api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '31.220.60.207',
+        port: '',
+        pathname: '',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- src/documents/Reusiable/MainCart/MainCart.tsx
+++ src/documents/Reusiable/MainCart/MainCart.tsx
@@ -8,9 +8,13 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import { setAddItemToCart, setSingleProduct } from "../../../redux/CartSlice"
 import { useDispatch } from "react-redux";
 import { useRouter } from 'next/router'
+import {useState, useEffect} from "react";
+import {apiAddress} from "../../../../config";
+import axios from "axios";
 
 const MainCart = ({mainData}: any) => {
     const classes = Styles();
+    const [data, setData] = useState<any | undefined>([])
     const query = useMediaQuery('@media(max-width: 650px)')
     const dispatch = useDispatch();
     const onAddToCart = (data: any) => {
@@ -22,20 +26,27 @@ const MainCart = ({mainData}: any) => {
         router.push({pathname: '/single-products', query: {id: data.id}})
     }
 
+    useEffect(() => {
+        axios.get(`${apiAddress}/product`).then((data) => {
+            setData(data.data.content)
+        }).catch(error => {
+            console.log(error)
+        })
+    }, [apiAddress, setData])
+
     return (
         <Box className={classes.mainCartWrapper}>
-            {mainData.map((item: any) =>
+            {data.map((item: any) =>
                 <Box className={classes.mainCart} key={item.id}>
                     <FavoriteBorderIcon className={classes.favoriteIcon}/>
                     <Box onClick={() => handleSingleProduct(item)}>
-                            {item.status === 'sale' ?
-                                <Typography className={classes.sale}>Top Sale</Typography> : item.status === 'new' ?
+                            {item.state === 'New' ?
+                                <Typography className={classes.sale}>Yangilik</Typography> : item.status === 'New' ?
                                     <Typography className={classes.new}>Yangilik</Typography> : null}
-                            <Image src={item.img} alt='image' width={query ? 120 : 180} height={query ? 120 : 180}
-                                   className={classes.mainImage}/>
+                            <Image src={item.photoUrl} alt='image' width={query ? 120 : 180} height={query ? 120 : 180} className={classes.mainImage}/>
                             <Typography className={classes.price}>{item.price} so&apos;m</Typography>
-                            <Typography className={classes.title}>{item.title}</Typography>
-                            <Typography className={classes.order}>{item.order} ta buyurtma</Typography>
+                            <Typography className={classes.title}>{item.name}</Typography>
+                            <Typography className={classes.order}>{item.phone}</Typography>
                     </Box>
                     <Box className={classes.footer}>
                         <Box onClick={() => router.push({pathname: '/single-products', query: {id: item.id}})}>
