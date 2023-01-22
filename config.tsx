f50903874f11e2dc85b92b3f5b2981c32ea60c79
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-01-22 20:14:17 +0500

    fix: build

--- config.tsx
+++ config.tsx
@@ -1,2 +1,1 @@
-export const apiAddress = 'http://31.220.60.207/nextstore/api'
-// export const apiAddress = process.env.NEXT_PUBLIC_SERVER_ADDRESS
+export const apiAddress = process.env.NEXT_PUBLIC_SERVER_ADDRESS
