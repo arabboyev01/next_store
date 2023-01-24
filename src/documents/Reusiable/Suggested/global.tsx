8dfe475751303690dfde22d7c1063d8a526458ba
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-01-24 12:00:09 +0500

    fix: improve

--- src/documents/Reusiable/Suggested/global.tsx
+++ src/documents/Reusiable/Suggested/global.tsx
@@ -1,4 +1,4 @@
-export function commafy( num: any ) {
+export function commafy( num: string ) {
     const str = num.toString().split('.');
     if (str[0].length >= 5) {
         str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1 ');
