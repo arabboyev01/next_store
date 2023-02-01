8f2fa7f878b21ef2bb4ffd35968c1767f7b9f364
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-02-01 18:28:09 +0500

    fix: api changes

--- src/documents/Reusiable/Navbar/User.tsx
+++ src/documents/Reusiable/Navbar/User.tsx
@@ -1,12 +1,17 @@
 import {Box} from "@mui/system";
 import Image from "next/image";
 import user_image from "../../../../public/assets/images/user.png"
+import {useRouter} from "next/router";
 const User = () => {
+    const router = useRouter();
+    const sendData = () => {
+        router.push({pathname: "/profile"})
+    }
     return(
-        <Box>
-            <Image src={user_image.src} alt='user image' width={40} height={40} style={{marginTop: "5px"}} />
+        <Box onClick={sendData}>
+            <Image src={user_image.src} alt='user image' width={40} height={40} style={{marginTop: "5px", cursor: 'pointer'}} />
         </Box>
     )
 }
 
-export default User
+export default User;
