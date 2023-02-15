0589c39e2e7bb380cbc83fcaa225e0cdd80a0c2b
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-02-16 00:20:38 +0500

    fix: some issues

---
+++ src/documents/Reusiable/SingleProduct/Dumb.tsx
@@ -0,0 +1,47 @@
+import { Box } from '@mui/system'
+import MainLoader from "../MainLoader/MainLoader"
+import PaymentTerm from '../Modal/PaymentTerm/PaymentTerm'
+import ProductDetail from '../SingleProduct/ProductDetail/ProductDetail'
+import ProductDescription from '../SingleProduct/ProductDescription/ProductDescription'
+import Suggested from '../Suggested/Suggested'
+import MainCart from '../MainCart/MainCart'
+import MainCarousel from '../MainCarousel/MainCarousel'
+import { SuggestedDataCarousel } from '../../DumbData/DumbData'
+import React from 'react'
+
+
+const Dumb = ({classes,handleCLose, single, handleOpen, query, suggestedData, open}: any) => (
+    <Box className={classes.singleProducts}>
+            {single.length === 0  ?
+                <Box className={classes.loader}>
+                    <MainLoader/>
+                </Box>:
+                <Box>
+                    <PaymentTerm open={open} handleCLose={handleCLose} price={single}/>
+                    <Box className={classes.productHeader}>
+                        <Box className={classes.imageWrapper}>
+                            <img src={`https://nextstore.in/nextstore${single.photoUrl}`} alt="product_image"
+                                 width={query ? 226 : 350}
+                                 height={query ? 216 : 350} className={classes.image}/>
+                        </Box>
+                        <Box>
+                            <ProductDetail handleOpen={handleOpen} data={single}/>
+                        </Box>
+                    </Box>
+                </Box>
+            }
+            <ProductDescription/>
+            <Suggested/>
+            <Box className={classes.suggested}>
+                <MainCart mainData={suggestedData} carousel="carousel"/>
+            </Box>
+            <Box className={classes.carousel}>
+                <MainCarousel
+                    data={SuggestedDataCarousel}
+                    height={query ? 433 : 320} bgColor="#FFE9BD" color="#000"
+                    sticky buttonText="Batafsil"
+                />
+            </Box>
+        </Box>
+)
+export default Dumb;
--- src/documents/Reusiable/SingleProduct/SingleProduct.tsx
+++ src/documents/Reusiable/SingleProduct/SingleProduct.tsx
@@ -1,78 +1,36 @@
-import { Box } from '@mui/system';
 import Styles from './singleproduct.style'
-import React, { useCallback, useEffect, useState } from 'react';
-import ProductDetail from './ProductDetail/ProductDetail';
+import React, { useEffect, useState } from 'react';
 import { useMediaQuery } from '@mui/material';
-import PaymentTerm from '../Modal/PaymentTerm/PaymentTerm';
-import { useSelector } from 'react-redux';
-import { selectSingleItem } from '../../../redux/CartSlice';
 import { useRouter } from 'next/router'
-import MainLoader from '../MainLoader/MainLoader';
-import Suggested from '../Suggested/Suggested';
-import ProductDescription from '../SingleProduct/ProductDescription/ProductDescription'
-import MainCarousel from '../MainCarousel/MainCarousel'
-import { SuggestedDataCarousel } from '../../DumbData/DumbData'
-import MainCart from '../MainCart/MainCart'
 import axios from 'axios'
 import { apiAddress } from '../../../../config'
+import Dumb from './Dumb'
 
 const SingleProduct = () => {
     const classes = Styles();
-    const singleProduct = useSelector(selectSingleItem);
-    const data = singleProduct[singleProduct.length - 1]
     const query = useMediaQuery('@media(max-width: 650px)');
     const [open, setOpen] = useState(false);
-    const {mainData} = useSelector((state: any) => state.cart);
     const [suggestedData, setSuggestData] = useState([])
     const router = useRouter()
     const {id} = router.query
     const handleOpen = () => setOpen(true);
     const handleCLose = () => setOpen(false);
-    const [filtered, setFiltered] = useState(data);
-
-    const fetchSingleData = useCallback(() => {
-        const fetchData = mainData[mainData?.length - 1]?.content?.find((item: any) => item.id == id)
-        setFiltered(fetchData)
-    }, [id, mainData, setFiltered]);
-
-    useEffect(() => {
-        fetchSingleData()
-    }, [fetchSingleData]);
+    const [single, setSingle] = useState([]);
 
     useEffect(() => {
         axios.get(`${apiAddress}/advertising`).then(res => setSuggestData(res.data)).catch(err => console.log(err))
-    }, [])
+        axios.get(`${apiAddress}/product/${id}`).then(res => setSingle(res.data)).catch(err => console.log(err))
+    }, [id])
 
     return (
-        <Box className={classes.singleProducts}>
-            {filtered === 'undefined' || !filtered ?
-                <Box className={classes.loader}>
-                    <MainLoader/>
-                </Box>:
-                <Box>
-                    <PaymentTerm open={open} handleCLose={handleCLose} price={filtered}/>
-                    <Box className={classes.productHeader}>
-                        <Box className={classes.imageWrapper}>
-                            <img src={`https://nextstore.in/nextstore${filtered.photoUrl}`} alt="product_image"
-                                 width={query ? 226 : 350}
-                                 height={query ? 216 : 350} className={classes.image}/>
-                        </Box>
-                        <Box>
-                            <ProductDetail handleOpen={handleOpen} data={filtered}/>
-                        </Box>
-                    </Box>
-                </Box>
-            }
-            <ProductDescription/>
-            <Suggested/>
-            <Box className={classes.suggested}>
-                <MainCart mainData={suggestedData} carousel="carousel"/>
-            </Box>
-            <Box className={classes.carousel}>
-                <MainCarousel data={SuggestedDataCarousel} height={query ? 433 : 320} bgColor="#FFE9BD" color="#000"
-                              sticky buttonText="Batafsil"/>
-            </Box>
-        </Box>
+        <Dumb classes={classes}
+              handleCLose={handleCLose}
+              single={single}
+              handleOpen={handleOpen}
+              query={query}
+              suggestedData={suggestedData}
+              open={open}
+        />
     )
 }
 export default SingleProduct
