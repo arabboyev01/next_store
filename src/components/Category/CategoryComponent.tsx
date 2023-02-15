87cfbf31f9d9a405bc9601dd52c3d6ddbf1cc7cd
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-02-16 01:00:59 +0500

    fix: some issues

--- src/components/Category/CategoryComponent.tsx
+++ src/components/Category/CategoryComponent.tsx
@@ -3,22 +3,22 @@ import { useMediaQuery} from "@mui/material";
 import { useCallback, useEffect, useState } from 'react'
 import { apiAddress } from '../../../config'
 import axios from "axios"
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 import Dumb from "./Dumb"
 
 const CategoryComponent = () => {
     const classes = style()
-    const router = useRouter()
-    const {id} = router.query
+    // const router = useRouter()
+    // const {id} = router.query
     const query = useMediaQuery('@media(max-width: 650px)')
 
     const [categoryData, setMainData] = useState([])
 
     const getDataByCategory = useCallback(() => {
-        axios.get(`${apiAddress}/category`).then((data) => {
+        axios.get(`${apiAddress}/category/`).then((data) => {
             setMainData(data.data)
         }).catch(err => console.log(err))
-    }, [apiAddress, id])
+    }, [])
 
     useEffect(() => {getDataByCategory()}, [getDataByCategory])
 
