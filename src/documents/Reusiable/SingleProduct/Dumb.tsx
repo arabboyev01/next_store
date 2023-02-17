be81f5d001fae47f417057d23c4046ebcf54afbc
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-02-17 22:47:27 +0500

    fix: filter section

--- src/documents/Reusiable/SingleProduct/Dumb.tsx
+++ src/documents/Reusiable/SingleProduct/Dumb.tsx
@@ -16,7 +16,7 @@ const Dumb = ({classes, handleCLose, single, handleOpen, query, suggestedData, o
                 <PaymentTerm open={open} handleCLose={handleCLose} price={single}/>
                 <Box className={classes.productHeader}>
                     <Box className={classes.imageWrapper}>
-                        <img src={`https://nextstore.in/nextstore/api/photo/download/${single.photosId}`}
+                        <img src={`https://nextstore.in/nextstore/api/photos/download/${single.photosId}`}
                              alt="product_image" width={query ? 226 : 350}
                              height={query ? 216 : 350} className={classes.image}
                         />
